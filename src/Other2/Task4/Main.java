26b5a6370d2e24ed0bd9645995829fb14d625618
Denis <33222627+denfranke@users.noreply.github.com>
2025-11-27 13:38:05 +0300

    Update Main.java

--- src/Other2/Task4/Main.java
+++ src/Other2/Task4/Main.java
@@ -1,10 +1,8 @@
-package Other2;
+package Other2.Task4;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
@@ -17,24 +15,52 @@ public class Main {
         return Math.abs(ans);
     }
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
         if (args.length == 0) {
-            System.out.println("Укажите имя файла.");
+            System.err.println("Ошибка: не указано имя файла");
             return;
         }
 
         String filename = args[0];
-        Scanner scanner = new Scanner(new File(filename));
+        File file = new File(filename);
+
+        if (!file.exists()) {
+            System.err.println("Ошибка: файл " + filename + " не найден.");
+            return;
+        }
+
+        if (!file.canRead()) {
+            System.err.println("Ошибка: нет доступа на чтение файла " + filename);
+            return;
+        }
 
         List<Integer> arr = new ArrayList<>();
-        int sum = 0, cnt = 0;
-        while (scanner.hasNext()) {
-            int a = Integer.parseInt(scanner.next());
-            arr.add(a);
-            sum += a;
-            cnt++;
+        try (Scanner scanner = new Scanner(file)) {
+            if (!scanner.hasNext()) {
+                System.err.println("Ошибка: файл пуст.");
+                return;
+            }
+
+            int sum = 0, cnt = 0;
+            while (scanner.hasNext()) {
+                if (scanner.hasNextInt()) {
+                    int a = scanner.nextInt();
+                    arr.add(a);
+                    sum += a;
+                    cnt++;
+                } else {
+                    System.err.println("Ошибка: обнаружено нецелочисленное значение в файле");
+                    return;
+                }
+            }
+
+            int ans = func(arr, sum / cnt);
+            System.out.println(ans > 20 ? "20 ходов недостаточно для приведения всех элементов массива к одному числу" : ans);
+
+        } catch (SecurityException e) {
+            System.err.println("Ошибка безопасности при доступе к файлу: " + e.getMessage());
+        } catch (Exception e) {
+            System.err.println("Произошла непредвиденная ошибка при чтении файла: " + e.getMessage());
         }
-        int ans = func(arr, sum / cnt);
-        System.out.println(ans>20?"20 ходов недостаточно для приведения всех элементов массива к одному числу":ans);
     }
 }
