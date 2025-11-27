4ba3f523b02de6d6fd286980e951c43b362226f9
Denis <33222627+denfranke@users.noreply.github.com>
2025-11-27 13:38:49 +0300

    Update Main.java

--- src/Other2/Task1/Main.java
+++ src/Other2/Task1/Main.java
@@ -1,19 +1,33 @@
-package Other2;
-
-import java.util.Scanner;
+package Other2.Task1;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        try {
+            if (args.length != 4) {
+                System.err.println("Использование: java Main <n1> <m1> <n2> <m2>\nПример: java Main 5 3 4 2");
+                return;
+            }
+            //Scanner scanner = new Scanner(System.in);
 
-        int n = scanner.nextInt();
-        int m = scanner.nextInt();
-        String path1 = func(n, m);
-        n = scanner.nextInt();
-        m = scanner.nextInt();
-        String path2 = func(n, m);
+            int n1 = Integer.parseInt(args[0]);
+            int m1 = Integer.parseInt(args[1]);
+            int n2 = Integer.parseInt(args[2]);
+            int m2 = Integer.parseInt(args[3]);
 
-        System.out.println(path1 + path2);
+            if (n1 <= 0 || m1 <= 0 || n2 <= 0 || m2 <= 0) {
+                System.err.println("Ошибка: все параметры должны быть положительными числами.");
+                return;
+            }
+
+            String path1 = func(n1, m1);
+            String path2 = func(n2, m2);
+
+            System.out.println(path1 + path2);
+        } catch (NumberFormatException e) {
+            System.err.println("Ошибка: все аргументы должны быть целыми числами.");
+        } catch (Exception e) {
+            System.err.println("Произошла непредвиденная ошибка: " + e.getMessage());
+        }
     }
 
     private static String func(int n, int m) {
